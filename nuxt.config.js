58f7b885683fe8363f3c4d267558153217b6b046
san randry <sinrandry@gmail.com>
2020-06-10 08:31:52 +0300

    add category

--- nuxt.config.js
+++ nuxt.config.js
@@ -98,8 +98,8 @@ export default {
    ** See https://axios.nuxtjs.org/options
    */
   axios: {
-    // baseURL: "http://localhost:3001/api"
-    baseURL: "https://rakitrabackend.herokuapp.com/api"
+    baseURL: "http://localhost:3001/api"
+    // baseURL: "https://rakitrabackend.herokuapp.com/api"
   },
 
   /**
